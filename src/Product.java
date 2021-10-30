c8ece676e2b5d489362ca1328a7e1d07b991d79a
Hi-Im-Jony <jccicai@yahoo.ie>
2021-10-30 22:06:47 +0100

    created product class

---
+++ src/Product.java
@@ -0,0 +1,15 @@
+
+/*
+A class to store information on a product
+*/
+public class Product {
+    String name;
+    int idCode;
+    float price;
+
+    Product(String name, int idCode, float price){
+        this.name = name;
+        this.idCode = idCode;
+        this.price = price;
+    }
+}
