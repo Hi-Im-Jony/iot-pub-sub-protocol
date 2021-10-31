ab40194aecc30bea3f179b2577390899f0f876dd
Hi-Im-Jony <jccicai@yahoo.ie>
2021-10-31 22:36:47 +0000

    basic printer class that can receive print command

--- src/Printer/Printer.java
+++ src/Printer/Printer.java
@@ -1,18 +1,60 @@
 package Printer;
 
+import java.io.IOException;
+
 import Product.Product;
+import SenderReceiver.SenderReceiver;
 
 /*
 A pseudo printer, will print item SEL's 
 Printer can:
 - Print item SEL
 */
-public class Printer {
+public class Printer{
+
+    private static class PrinterReceiver extends Thread{
+        SenderReceiver receiver;
+        int receiverPort;
+
+        public PrinterReceiver(int receiverPort) throws IOException{
+            this.receiverPort = receiverPort;
+            receiver = new SenderReceiver(receiverPort, "Printer");
+        }
+
+        public String receive() throws IOException{
+            return receiver.receive();
+        }
+
+        @Override
+        public void run(){
+            try {
+                String data = receiver.receive();
+                PrinterReceiver backup = new PrinterReceiver(receiverPort);
+                backup.start();
+                printSEL(data, 0.0);
+                
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
     
-    private void printSEL(Product product){
-        System.out.println("*********************************");
-        System.out.println("          Name:"+product.name+"               ");
-        System.out.println("          Price: "+product.price+"               ");
-        System.out.println("*********************************");
+    public static void main(String[] args) throws IOException {
+        
+        int receiverPort =  Integer.parseInt(args[0]);
+        PrinterReceiver receiver = new PrinterReceiver(receiverPort);
+
+        String data = receiver.receive(); // receive data
+        PrinterReceiver backup = new PrinterReceiver(receiverPort); // create new "back up thread" to receive while we print
+        backup.start();
+        printSEL(data, 0.0);
+    }
+
+    private static void printSEL(String name, double price){
+        System.out.println("*********************************"
+                        +"\n          Name:"+name+"               "
+                        +"\n          Price: "+price+"               "
+                        +"\n*********************************");
     }
 }
