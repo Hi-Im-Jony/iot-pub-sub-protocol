6ea2528a6790d1511959c7445ff975b55b7e1ae4
Hi-Im-Jony <jccicai@yahoo.ie>
2021-10-31 23:35:31 +0000

    changing broker so i can test printer

--- src/Broker/Broker.java
+++ src/Broker/Broker.java
@@ -1,16 +1,10 @@
 package Broker;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.ObjectInputStream;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
 
 import java.util.HashMap;
-
+import java.io.IOException;
 import java.util.ArrayList;
+
+import SenderReceiver.SenderReceiver;
 /* 
 The broker is the middle man in this network. 
 It receives packets with requests from various clients, and tries to fulfill those requests.
@@ -24,88 +18,21 @@ public class Broker {
     
 	static final int MTU = 1500;
 
-	static DatagramSocket receiverSocket;
-	static DatagramSocket senderSocket;
+    static SenderReceiver sender;
 
     static HashMap<Integer, ArrayList<Integer>> topicSubscribers;
+
     public static void main(String[] args) throws IOException {
 
-        System.out.println("ReceiverProcess - Program start");
+        sender = new SenderReceiver(0, "Broker"); // hardcoded receiver port because there is only one broker
+        
+        for(int i = 0; i<10;i++){
+            String payload = sender.buildPayload("Random Topic", "Random Data");
+            sender.send(payload, 2);
+        }
 
-		InetAddress address= InetAddress.getLocalHost(); 
-
-		int receiverPort =  Integer.parseInt(args[0]);
-        int destinationPort = Integer.parseInt(args[1]);
-
-        String topic = args[2];
-		String data = args[2];
-		
-		senderSocket = new DatagramSocket();
-			
-		receiverSocket= new DatagramSocket(receiverPort, address);
-		receiverSocket.setSoTimeout(500); // 0.5 seconds
-
-        topicSubscribers = new HashMap<Integer, ArrayList<Integer>>();
-
-        subscibe(1, 3);
-        subscibe(1, 2);
-        subscibe(1, 1);
-		while(true){
-            
-			try {
-                if(receiverPort>0)
-                    receive();
-            } catch (IOException e) {
-                System.out.println("Nothing received" );
-            }
-            if(destinationPort>0)
-				send(buildPayload(topic, data), destinationPort);	
-            		
-		}
+       
     }
-
-    private static void receive() throws IOException{
-		
-		// create buffer for data, packet and receiverSocket
-		byte[] buffer= new byte[MTU];
-		DatagramPacket packet= new DatagramPacket(buffer, buffer.length);
-	
-		receiverSocket.receive(packet);
-
-		// extract data from packet
-		buffer= packet.getData();
-		ByteArrayInputStream bstream= new ByteArrayInputStream(buffer);
-		ObjectInputStream  ostream= new ObjectInputStream(bstream);
-
-		// print data and end of program
-        String data =  ostream.readUTF();
-
-		//System.out.println("Data: " + data);
-
-        executeRequest(packet.getPort(), data);
-	}
-
-    private static void send(String payload, int dest) throws IOException{
-		
-		InetAddress address= InetAddress.getLocalHost();   
-		int port= dest;                       
-	
-		ByteArrayOutputStream bstream= new ByteArrayOutputStream();
-		ObjectOutputStream ostream= new ObjectOutputStream(bstream);
-
-		ostream.writeUTF(payload);
-		ostream.flush();
-		
-		byte[] buffer = bstream.toByteArray();
-		// create packet addressed to destination
-		DatagramPacket packet= new DatagramPacket(buffer, buffer.length, address, port);
-		senderSocket.send(packet);
-		
-	}
-
-	private static String buildPayload(String topic, String data){
-		return topic + data;
-	}
 
     private static void executeRequest(int requestor_port, String data) throws IOException{
         int request = data.charAt(0) - 48; // turn ASCII value into int value
@@ -157,13 +84,6 @@ public class Broker {
     }
 
     private static void publish(int topic, String data) throws IOException{
-       // System.out.println("Executing 'publish'");
-        ArrayList<Integer> subs = topicSubscribers.get(topic);
-        if(subs != null){
-            String payload = buildPayload((Integer.toString(topic)), data);
-            for(int i = 0; i< subs.size(); i ++){
-                send(payload, subs.get(i));
-            }
-        }
+       // TODO publish data to subs of topic
     }
 }
