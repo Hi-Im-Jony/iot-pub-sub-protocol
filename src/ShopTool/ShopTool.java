aaeb02a56f0411e998e91b82f60850c2d30280e6
Hi-Im-Jony <jccicai@yahoo.ie>
2021-11-02 18:25:26 +0000

    starting work on ShopTool

---
+++ src/ShopTool/ShopTool.java
@@ -0,0 +1,15 @@
+package ShopTool;
+
+/*
+A pseudo tool used by workers in a shop to check prices of items, 
+get notified of product changes, and print product SEL
+
+ShopTool can:
+- "Scan" qr/bar codes to check item details
+- Print item SEL
+- Subscribe to a "section" of the store (ie, sub to a topic)
+- Get notified when item in section gets added/edited/removed
+*/
+public class ShopTool {
+    
+}
