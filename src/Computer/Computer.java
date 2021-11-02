77a67e19784fd9e2c737a507c49b4806989486cd
Hi-Im-Jony <jccicai@yahoo.ie>
2021-11-02 10:18:53 +0000

    checking if computer should be manualy operated

--- src/Computer/Computer.java
+++ src/Computer/Computer.java
@@ -28,8 +28,7 @@ public class Computer {
         CopmuterReceiverThread receiverThread = new CopmuterReceiverThread(); // create new "back up thread" to receive while we print
         receiverThread.start();
 
-        //if(Integer.parseInt(args[1])==0)
-        if(false)
+        if(Integer.parseInt(args[1])==0)
             operateManually();
         else
             operateAutomaticlly();
