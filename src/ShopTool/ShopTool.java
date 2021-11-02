06d09fc6d0d03114fdd79b996ac6647e0d8d1839
Hi-Im-Jony <jccicai@yahoo.ie>
2021-11-02 18:43:05 +0000

    implemented most of ShopTool

--- src/ShopTool/ShopTool.java
+++ src/ShopTool/ShopTool.java
@@ -9,6 +9,7 @@ import java.io.ObjectInputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.concurrent.TimeUnit;
 /*
 A pseudo tool used by workers in a shop to check prices of items, 
 get notified of product changes, and print product SEL
@@ -22,7 +23,69 @@ ShopTool can:
 public class ShopTool {
     
 
-    Transreceiver transreceiver;
+    static final int BROKER_PORT = 2;
+
+    private static Transreceiver transreceiver;
+    private static String cache;
+    public static void main(String[] args) throws IOException, InterruptedException {
+        
+        System.out.println("ShopTool turned on");
+
+        transreceiver = new Transreceiver();
+
+        ReceiverThread receiverThread = new ReceiverThread(); // create new "back up thread" to receive while we print
+        receiverThread.start();
+
+    }
+
+    private static class ReceiverThread extends Thread{
+        @Override
+        public void run(){
+            try {
+                String data = transreceiver.receive();
+
+                ReceiverThread receiverThread = new ReceiverThread();
+                receiverThread.start();
+
+                interpret(data);
+                
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void interpret(String data){
+        String[] splitData = data.split(":"); // data = dataType:info
+        String dataType = splitData[0];
+
+        switch(dataType){
+            case "prod": // data represents a product
+                cache = splitData[1];
+                break;
+            case "not": // data is a notification
+                System.out.println("NOTIFICATION: "+splitData[1]);
+                break;
+        }
+    }
+
+    private static void scan(int idCode) throws IOException, InterruptedException{
+        transreceiver.send("reqprod:"+idCode, BROKER_PORT);
+        TimeUnit.MILLISECONDS.sleep(500); // allow time to receive, equivalent to a "loading" screen
+    }
+
+    private static void printProductSEL(String info) throws IOException{
+        if(info!=null)
+            transreceiver.send("print:"+info, BROKER_PORT);
+    }
+    
+    private static void sub(String topic){
+
+    }
+
+    private static void unsub(String topic){
+        
+    }
 
     // Class that can send and/or receive udp packets
     private static class Transreceiver{
