3c8b7ca7210f61a2283dfd30c2461e6569f7300a
Hi-Im-Jony <jccicai@yahoo.ie>
2021-11-02 18:27:22 +0000

    added Transreceiver class and required imports

--- src/ShopTool/ShopTool.java
+++ src/ShopTool/ShopTool.java
@@ -1,5 +1,14 @@
 package ShopTool;
 
+
+import java.io.IOException;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 /*
 A pseudo tool used by workers in a shop to check prices of items, 
 get notified of product changes, and print product SEL
@@ -12,4 +21,53 @@ ShopTool can:
 */
 public class ShopTool {
     
+
+    Transreceiver transreceiver;
+
+    // Class that can send and/or receive udp packets
+    private static class Transreceiver{
+        
+        static final int MTU = 1500;
+
+        private DatagramSocket transreceiver;
+        
+        public Transreceiver() throws IOException{
+            transreceiver= new DatagramSocket();
+        }
+
+        public String receive() throws IOException{
+            
+            // create buffer for data, packet and transreceiver
+            byte[] buffer= new byte[MTU];
+            DatagramPacket packet= new DatagramPacket(buffer, buffer.length);
+        
+            transreceiver.receive(packet);
+
+            // extract data from packet
+            buffer= packet.getData();
+            ByteArrayInputStream bstream= new ByteArrayInputStream(buffer);
+            ObjectInputStream  ostream= new ObjectInputStream(bstream);
+
+            String data =  ostream.readUTF();
+            System.out.println("Received: \""+data+",\" from port:"+packet.getPort());
+            return data;
+        }
+
+        public void send(String payload, int dest) throws IOException{
+            
+            InetAddress address= InetAddress.getLocalHost();   
+            int port= dest;                       
+        
+            ByteArrayOutputStream bstream= new ByteArrayOutputStream();
+            ObjectOutputStream ostream= new ObjectOutputStream(bstream);
+
+            ostream.writeUTF(payload);
+            ostream.flush();
+            
+            byte[] buffer = bstream.toByteArray();
+            // create packet addressed to destination
+            DatagramPacket packet= new DatagramPacket(buffer, buffer.length, address, port);
+            transreceiver.send(packet);
+        }
+    }
 }
